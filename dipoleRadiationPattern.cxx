3bee148fbc73ac1295b0901171b03bba04e10a2f
Seb Jones <jonesseb100@gmail.com>
2021-10-21 15:56:43 +0100

    Initial commit of an executable

---
+++ dipoleRadiationPattern.cxx
@@ -0,0 +1,10 @@
+// dipoleRadiationPattern.cxx
+// Basic script for generating components of the electric field
+#include <iostream>
+
+int main()
+{
+  std::cout<<"All done"<<std::endl;
+  
+  return 0;
+}
