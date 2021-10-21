ea3dc7201db9c40ff979cdc92f777b7cebfe5ec7
Seb Jones <jonesseb100@gmail.com>
2021-10-21 17:45:43 +0100

    Figured out a method of linking the ROOT libraries

--- dipoleRadiationPattern.cxx
+++ dipoleRadiationPattern.cxx
@@ -1,9 +1,15 @@
 // dipoleRadiationPattern.cxx
 // Basic script for generating components of the electric field
+
+// STL
 #include <iostream>
+
+// ROOT includes
+#include "TVector3.h"
 
 int main()
 {
+  TVector3 vec(0.0, 0.0, 0.0);
   std::cout<<"All done"<<std::endl;
   
   return 0;
