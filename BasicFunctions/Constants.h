b4010d14ab6b0b95c5d417674734b3116c493b64
Seb Jones <jonesseb100@gmail.com>
2021-10-29 13:17:50 +0100

    const to constexpr

--- BasicFunctions/Constants.h
+++ BasicFunctions/Constants.h
@@ -6,8 +6,8 @@
 
 namespace rad
 {
-  const double EPSILON0 = 8.8541878128e-12;
-  const double MU0 = 1.25663706212e-6;
+  constexpr double EPSILON0 = 8.8541878128e-12;
+  constexpr double MU0 = 1.25663706212e-6;
 }
  
 #endif
