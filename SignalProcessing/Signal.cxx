c9ba4d54d8907a4b22edffb2eddf823fa16d93a5
Seb Jones <jonesseb100@gmail.com>
2021-11-04 18:59:31 +0000

    Added signal processing to function. Entailed some removing of the noise stuff from FieldPoint

--- SignalProcessing/Signal.cxx
+++ SignalProcessing/Signal.cxx
@@ -1,15 +1,77 @@
 // Signal.cxx
 
 #include "SignalProcessing/Signal.h"
+#include "SignalProcessing/LocalOscillator.h"
+#include "FieldClasses/FieldClasses.h"
+#include "BasicFunctions/BasicFunctions.h"
+
+#include <vector>
+#include <stdio>
 
 #include "TGraph.h"
 
+#include "FFTtools.h"
+
 rad::Signal::Signal() {
-  grSignalTime = new TGraph();
+  grVITime = new TGraph();
+  grVQTime = new TGraph();
 }
 
 rad::Signal::~Signal() {
-  delete grSignalTime;
+  delete grVITime;
+  delete grVQTime;
 }
 
+rad::Signal::Signal(FieldPoint fp, LocalOscillator lo, double srate,
+		    std::vector<GaussianNoise> noiseTerms, const bool kUseRetardedTime) {
+  sampleRate = srate;
 
+  // Create the signal tgraph in the time domain
+  TGraph* grVITimeUnfiltered = new TGraph();
+  TGraph* grVQTimeUnfiltered = new TGraph();
+  TGraph* grInputVoltage = fp.GetDipoleLoadVoltageTimeDomain(kUseRetardedTime, -1, -1);
+  std::cout<<"Performing the downmixing..."<<std::endl;
+  for (int i = 0; i < grInputVoltage->GetN(); i++) {
+    grVITimeUnfiltered->SetPoint(i, grInputVoltage->GetPointX(i), grInputVoltage->GetPointY(i)*lo.GetInPhaseComponent(grInputVoltage->GetPointX(i)));
+    grVQTimeUnfiltered->SetPoint(i, grInputVoltage->GetPointX(i), grInputVoltage->GetPointY(i)*lo.GetQuadratureComponent(grInputVoltage->GetPointX(i)));
+  }
+
+  // Now we need to filter and then sample these signals
+  std::cout<<"Filtering.."<<std::endl;
+  TGraph* grVITimeUnsampled = FFTtools::simplePassBandFilter(grVITimeUnfiltered, 0.0, sampleRate/2.0);
+  TGraph* grVQTimeUnsampled = FFTtools::simplePassBandFilter(grVQTimeUnfiltered, 0.0, sampleRate/2.0);
+
+  // Now do sampling
+  // Use simple linear interpolation for the job
+  grVITime = new TGraph();
+  grVQTime = new TGraph();  
+  setGraphAttr(grVITime);
+  setGraphAttr(grVQTime);
+
+  std::cout<<"Sampling..."<<std::endl;
+  double sampleSpacing = 1.0 / sampleRate;
+  double sampleTime = grVITimeUnsampled->GetPointX(0);
+  for (int i = 0; i < grVITimeUnsampled->GetN(); i++) {
+    double time = grVITimeUnsampled->GetPointX(i);
+    if (time < sampleTime) continue;
+    else if (i == 0) {
+      grVITime->SetPoint(grVITime->GetN(), sampleTime, grVITimeUnsampled->GetPointY(0));
+      grVQTime->SetPoint(grVQTime->GetN(), sampleTime, grVQTimeUnsampled->GetPointY(0));
+      sampleTime += sampleSpacing;
+    }
+    else {
+      // Sample the distribution
+      double calcVI = grVITimeUnsampled->GetPointY(i-1) + (sampleTime - grVITimeUnsampled->GetPointX(i-1)) * (grVITimeUnsampled->GetPointY(i) - grVITimeUnsampled->GetPointY(i-1)) / (time - grVITimeUnsampled->GetPointX(i-1));
+      double calcVQ = grVQTimeUnsampled->GetPointY(i-1) + (sampleTime - grVQTimeUnsampled->GetPointX(i-1)) * (grVQTimeUnsampled->GetPointY(i) - grVQTimeUnsampled->GetPointY(i-1)) / (time - grVQTimeUnsampled->GetPointX(i-1));
+      grVITime->SetPoint(grVITime->GetN(), sampleTime, calcVI);
+      grVQTime->SetPoint(grVQTime->GetN(), sampleTime, calcVQ);
+      sampleTime += sampleSpacing;      
+    }
+  }
+  
+  delete grInputVoltage;
+  delete grVITimeUnfiltered;
+  delete grVQTimeUnfiltered;
+  delete grVITimeUnsampled;
+  delete grVQTimeUnsampled;
+}
--- SignalProcessing/Signal.h
+++ SignalProcessing/Signal.h
@@ -6,21 +6,38 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#include "SignalProcessing/LocalOscillator.h"
+#include "SignalProcessing/NoiseFunc.h"
+#include "FieldClasses/FieldClasses.h"
+#include "BasicFunctions/BasicFunctions.h"
+
+#include <vector>
+
 #include "TGraph.h"
 
 namespace rad
 {
   /// Signal base class
-  /// Class members are a voltage signal in the time domain
+  /// Class members are a voltage signal in the time domain and an out of phase component 
 
+  
   class Signal
   {
   private:
-    TGraph* grSignalTime;
-
+    TGraph* grVITime; // In phase component
+    TGraph* grVQTime; // Quadrature component
+    double sampleRate;
+    
   public:
     Signal();
     ~Signal();
+
+    /// \param fp The field point where the antenna is located
+    /// \param lo The local oscillator used to do the downmixing
+    /// \param srate The sample rate of a hypothetical ADC
+    /// \param noiseTerms A vector of noise terms to be added to the signal
+    /// \param kUseRetardedTime Boolean on whether or not to use the retarded time
+    Signal(FieldPoint fp, LocalOscillator lo, double srate, std::vector<GaussianNoise> noiseTerms={}, const bool kUseRetardedTime=false);
   };
 }
 
