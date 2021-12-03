e094c79bf83f0b45e0ef6287f55f3520246cdc79
Seb Jones <jonesseb100@gmail.com>
2021-12-03 11:06:26 +0000

    Added elecron rest mass

--- BasicFunctions/Constants.h
+++ BasicFunctions/Constants.h
@@ -8,6 +8,9 @@ namespace rad
 {
   constexpr double EPSILON0 = 8.8541878128e-12;
   constexpr double MU0 = 1.25663706212e-6;
+
+  // Electron rest mass in kg
+  constexpr double ME = 9.1093837015e-31;
 }
  
 #endif
