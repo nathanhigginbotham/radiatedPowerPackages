d5b70aa4e8ee902f8372328a47dab67fea422f44
Seb Jones <jonesseb100@gmail.com>
2021-12-03 14:52:28 +0000

    Move function to public so that it may be call by an executable

--- ElectronDynamics/BorisSolver.h
+++ ElectronDynamics/BorisSolver.h
@@ -42,6 +42,16 @@ namespace rad
     /// \returns a 3-vector of the acceleration 
     TVector3 acc(const TVector3 pos, const TVector3 vel);
 
+    /// Returns the B field at the position
+    TVector3 calc_b_field(const TVector3 pos);
+    
+  public:
+    /// \param field_v Pointer to a magnetic field calculator
+    /// \param charge_v Particle charge. Default is electron charge
+    /// \param mass_v Particle mass. Default is electron mass
+    /// \param tau_v Energy loss. Default is zero
+    BorisSolver(BaseField* field_v, const double charge_v=-TMath::Qe(), const double mass_v=ME, const double tau_v=0.0);
+    
     /// Advances position and velocity vector by a set time
     /// \param time_step The time step over which to advance the charge's motion
     /// \param x0 Vector of the charge's starting position
@@ -50,16 +60,6 @@ namespace rad
     std::tuple<TVector3, TVector3> advance_step(const double time_step,
 						const TVector3 x0, const TVector3 v0);
 
-    /// Returns the B field at the position
-    TVector3 calc_b_field(const TVector3 pos);
-    
-  public:
-
-    /// \param field_v Pointer to a magnetic field calculator
-    /// \param charge_v Particle charge. Default is electron charge
-    /// \param mass_v Particle mass. Default is electron mass
-    /// \param tau_v Energy loss. Default is zero
-    BorisSolver(BaseField* field_v, const double charge_v=-TMath::Qe(), const double mass_v=ME, const double tau_v=0.0);
   };
 }
 
