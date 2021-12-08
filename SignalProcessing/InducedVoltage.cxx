322757f4ad3093aae93e9e3f368c11fab120fb4a
Seb Jones <jonesseb100@gmail.com>
2021-12-08 13:44:07 +0000

    Corrected while loop so that it now ends at the correct time

--- SignalProcessing/InducedVoltage.cxx
+++ SignalProcessing/InducedVoltage.cxx
@@ -18,9 +18,27 @@ rad::InducedVoltage::InducedVoltage(TString trajectoryFilePath, IAntenna* myAnte
   FieldPoint fp(trajectoryFilePath, myAntenna);
   if (minTime == -1) minTime = 0.0;
   if (maxTime == -1) maxTime = fp.GetFinalTime();
-  fp.GenerateFields(minTime, maxTime);
+
+  // To avoid running out of memory, generate the fields in more manageable chunks
+  // Avoids having massive versions of unnecessary graphs
+  const double chunkSize = 25e-6;
+  double thisChunk = minTime + chunkSize;
+  double lastChunk = minTime;
+  grVoltage = new TGraph();
   std::cout<<"Generating voltages"<<std::endl;
-  grVoltage = fp.GetAntennaLoadVoltageTimeDomain(kUseRetardedTime);
+  while (thisChunk <= maxTime && thisChunk != lastChunk) {
+    fp.GenerateFields(lastChunk, thisChunk);
+    TGraph* voltageTemp = fp.GetAntennaLoadVoltageTimeDomain(kUseRetardedTime);
+    // Now write this to the main voltage graph
+    std::cout<<"Writing to main voltage graph"<<std::endl;
+    for (int i = 0; i < voltageTemp->GetN(); i++) {
+      grVoltage->SetPoint(grVoltage->GetN(), voltageTemp->GetPointX(i), voltageTemp->GetPointY(i));
+    }
+    delete voltageTemp;
+    lastChunk = thisChunk;
+    thisChunk += chunkSize;
+    if (thisChunk > maxTime) thisChunk = maxTime;
+  }
 }
 
 rad::InducedVoltage::InducedVoltage(const InducedVoltage &iv) {
