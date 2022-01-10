0fd3f7bf0f65087fe646f5bf62a27b5b70821863
Seb Jones <jonesseb100@gmail.com>
2022-01-10 14:49:30 +0000

    Modifications to comments

--- SignalProcessing/Signal.h
+++ SignalProcessing/Signal.h
@@ -103,28 +103,28 @@ namespace rad
 
     /// Returns the power spectrum of the in-phase voltage component after all signal processing
     /// \param loadResistance The load resistance used for the power calculation
-    /// \param firstPoint The first point to return
-    /// \param lastPoint The last point to return
+    /// \param firstPoint The first point in the time series to use
+    /// \param lastPoint The last point in the time series to use
     TGraph* GetVIPowerNorm(const double loadResistance, int firstPoint=-1, int lastPoint=-1);
 
     /// Returns the power spectrum of the quadrature voltage component after all signal processing
     /// \param loadResistance The load resistance used for the power calculation
-    /// \param firstPoint The first point to return
-    /// \param lastPoint The last point to return
+    /// \param firstPoint The first point in the time series to use
+    /// \param lastPoint The last point in the time series to use
     TGraph* GetVQPowerNorm(const double loadResistance, int firstPoint=-1, int lastPoint=-1);
 
     /// Returns the power spectrum of the in-phase voltage component after all signal processing
     /// Power spectrum in this case is the periodogram
     /// \param loadResistance The load resistance used for the power calculation
-    /// \param firstPoint The first point to return
-    /// \param lastPoint The last point to return
+    /// \param firstPoint The first point in the time series to use
+    /// \param lastPoint The last point in the time series to use
     TGraph* GetVIPowerPeriodogram(const double loadResistance, int firstPoint=-1, int lastPoint=-1);
 
     /// Returns the power spectrum of the quadrature voltage component after all signal processing
     /// Power spectrum in this case is the periodogram
     /// \param loadResistance The load resistance used for the power calculation
-    /// \param firstPoint The first point to return
-    /// \param lastPoint The last point to return
+    /// \param firstPoint The first point in the time series to use
+    /// \param lastPoint The last point in the time series to use
     TGraph* GetVQPowerPeriodogram(const double loadResistance, int firstPoint=-1, int lastPoint=-1);
 
     /// Returns the 2D spectrogram made using the in-phase voltage component
