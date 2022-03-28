7d1df57e865a967003fe67aa32014e25fec6269a
Seb Jones <jonesseb100@gmail.com>
2022-03-28 14:12:42 +0100

    Moved EM functions to their own header file to reduce clutter

--- BasicFunctions/BasicFunctions.h
+++ BasicFunctions/BasicFunctions.h
@@ -15,46 +15,7 @@
 #include <vector>
 
 namespace rad
-{
-  /// Calculates electric field from a moving electron at a point
-  /// Coordinates are all in same reference framce
-  /// \param fieldPoint Vector of field point
-  /// \param ePosition Vector of electron position
-  /// \param eVelocity Electron velocity vector
-  /// \param eAcceleration Electron acceleration vector
-  /// \Returns The electric field vector
-  ROOT::Math::XYZVector CalcEField(const ROOT::Math::XYZPoint fieldPoint, const ROOT::Math::XYZPoint ePosition, const ROOT::Math::XYZVector eVelocity, const ROOT::Math::XYZVector eAcceleration);
-  /// Calculates electric field using TVector3 framework
-  ROOT::Math::XYZVector CalcEField(const TVector3 fieldPoint, const TVector3 ePosition, const TVector3 eVelocity, const TVector3 eAcceleration);
-  
-  /// Calculates magnetic field from a moving electron at a point
-  /// Coordinates are all in same reference framce
-  /// \param fieldPoint Vector of field point
-  /// \param ePosition Vector of electron position
-  /// \param eVelocity Electron velocity vector
-  /// \param eAcceleration Electron acceleration vector
-  /// \Returns The magnetic field vector
-  ROOT::Math::XYZVector CalcBField(const ROOT::Math::XYZPoint fieldPoint, const ROOT::Math::XYZPoint ePosition, const ROOT::Math::XYZVector eVelocity, const ROOT::Math::XYZVector eAcceleration);
-  /// Calculates magnetic field using TVector3 framework
-  ROOT::Math::XYZVector CalcBField(const TVector3 fieldPoint, const TVector3 ePosition, const TVector3 eVelocity, const TVector3 eAcceleration); 
-
-  /// Calculates Poynting vector from a moving electron at a point
-  /// Coordinates are all in same reference framce
-  /// \param fieldPoint Vector of field point
-  /// \param ePosition Vector of electron position
-  /// \param eVelocity Electron velocity vector
-  /// \param eAcceleration Electron acceleration vector
-  /// \Returns The Poynting vector
-  ROOT::Math::XYZVector CalcPoyntingVec(const ROOT::Math::XYZPoint fieldPoint, const ROOT::Math::XYZPoint ePosition, const ROOT::Math::XYZVector eVelocity, const ROOT::Math::XYZVector eAcceleration);
-  /// Calculates Poynting vector from a moving electron at a point
-  ROOT::Math::XYZVector CalcPoyntingVec(const TVector3 fieldPoint, const TVector3 ePosition, const TVector3 eVelocity, const TVector3 eAcceleration);
-  
-  /// Calculates Poynting vector from electromagnetic field vectors at a point
-  /// \param EField Electric field vector
-  /// \param BField Magnetic field vector
-  /// \Returns The Poynting vector
-  ROOT::Math::XYZVector CalcPoyntingVec(const ROOT::Math::XYZVector EField, const ROOT::Math::XYZVector BField);
-  
+{  
   void setGraphAttr(TGraph *gr);
   
   void SetHistAttr(TH1 *h);
