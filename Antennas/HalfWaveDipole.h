de78b2477b317832abd0585af31d95ee574060f4
Seb Jones <jonesseb100@gmail.com>
2022-05-16 17:00:12 +0100

    Minor clarification

--- Antennas/HalfWaveDipole.h
+++ Antennas/HalfWaveDipole.h
@@ -20,7 +20,7 @@ namespace rad {
     /// \param antXAx Antenna defined X axis
     /// \param antZAx Antenna defined Z axis
     /// \param freq Central frequency of the antenna
-    /// \param delay Time delay of this antenna
+    /// \param delay Time delay of this antenna (in seconds)
     HalfWaveDipole(TVector3 antPos, TVector3 antXAx, TVector3 antZAx, double freq, double delay=0.0);
     
     TVector3 GetETheta(const TVector3 electronPosition);
