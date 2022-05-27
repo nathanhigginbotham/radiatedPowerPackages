cf1fb9fc523c9edff211afee2e278f815aec41ad
Seb Jones <jonesseb100@gmail.com>
2022-05-27 10:36:09 +0100

    Similarly update the NuFit values to use inline

--- BasicFunctions/NuFitValues.h
+++ BasicFunctions/NuFitValues.h
@@ -12,21 +12,21 @@
 
 namespace rad
 {
-  constexpr double kNuFitDmsq21 = 7.42e-5;
+  inline constexpr double kNuFitDmsq21 { 7.42e-5 };
   
   // Assuming normal hierarchy
-  constexpr double kNuFitTh12NH = 33.44 * TMath::Pi() / 180.0;
-  constexpr double kNuFitTh23NH = 49.2 * TMath::Pi() / 180.0;
-  constexpr double kNuFitTh13NH = 8.57 * TMath::Pi() / 180.0;
-  constexpr double kNuFitdCPNH  = 194 * TMath::Pi() / 180.0;
-  constexpr double kNuFitDmsq32NH = 2.515e-3 - kNuFitDmsq21;
+  inline constexpr double kNuFitTh12NH { 33.44 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitTh23NH { 49.2 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitTh13NH { 8.57 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitdCPNH  { 194 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitDmsq32NH { 2.515e-3 - kNuFitDmsq21 };
       
   // Assuming inverted hierarchy
-  constexpr double kNuFitTh12IH = 33.45 * TMath::Pi() / 180.0;
-  constexpr double kNuFitTh23IH = 49.5 * TMath::Pi() / 180.0;
-  constexpr double kNuFitTh13IH = 8.60 * TMath::Pi() / 180.0;
-  constexpr double kNuFitdCPIH  = 287 * TMath::Pi() / 180.0;
-  constexpr double kNuFitDmsq32IH = -2.498e-3;
+  inline constexpr double kNuFitTh12IH { 33.45 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitTh23IH { 49.5 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitTh13IH { 8.60 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitdCPIH  { 287 * TMath::Pi() / 180.0 };
+  inline constexpr double kNuFitDmsq32IH { -2.498e-3 };
 }
 
 #endif
