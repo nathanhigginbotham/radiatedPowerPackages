7c99deeb6086cd5f7796a4c972674d342e4436f1
Seb Jones <jonesseb100@gmail.com>
2022-11-24 13:27:50 +0000

    Some improvements to the patch antenna class

--- Antennas/PatchAntenna.cxx
+++ Antennas/PatchAntenna.cxx
@@ -7,51 +7,59 @@
 #include "TVector3.h"
 #include "TMath.h"
 
-rad::PatchAntenna::PatchAntenna(TVector3 antPos, TVector3 antXAx, TVector3 antZAx,
-				const double length, const double width, const double permittivity, double delay) {
+rad::PatchAntenna::PatchAntenna(TVector3 antPos, TVector3 antXAx, TVector3 antYAx,
+                                double width, double height, double f0,
+                                double permittivity, double delay)
+{
+  antYAx = antYAx.Unit();
   antXAx = antXAx.Unit();
-  antZAx = antZAx.Unit();
 
   // Make sure axes are perpendicular
-  assert(antXAx.Dot(antZAx) == 0);
+  assert(antXAx.Dot(antYAx) == 0);
 
   antennaPosition = antPos;
   antennaXAxis = antXAx;
-  antennaZAxis = antZAx;
-  antennaYAxis = antZAx.Cross(antXAx);
-  
-  L = length;
+  antennaYAxis = antYAx;
+  antennaZAxis = antXAx.Cross(antYAx);
+
+  H = height;
   W = width;
+
   relativePerm = permittivity;
-  centralFreq = TMath::C() / (2.0 * L * TMath::Sqrt(relativePerm));
+  centralFreq = f0;
   timeDelay = delay;
-  
+
+  // First calculate the effective perimittivity
+  double epEff{((permittivity + 1) / 2) + ((permittivity - 1) / 2) * pow(1 + 12 * H / W, -0.5)};
+  // Then calculate the effective length
+  LEff = TMath::C() / (2 * f0 * sqrt(epEff));
+  // From this, we can calculate the actual length of the patch
+  double deltaL{0.412 * H * (epEff + 0.3) * (W / H + 0.264) / ((epEff - 0.258) * (W / H + 0.8))};
+  L = LEff - 2 * deltaL;
+
   SetBandwidth();
 }
 
-TVector3 rad::PatchAntenna::GetETheta(const TVector3 electronPosition) {
+TVector3 rad::PatchAntenna::GetETheta(const TVector3 electronPosition)
+{
   TVector3 thetaHat = GetThetaHat(electronPosition);
   double thetaAng = GetTheta(electronPosition);
   double phiAng = GetPhi(electronPosition);
   const double k = 2 * TMath::Pi() * centralFreq / TMath::C();
 
-  double premult = TMath::Sin( k*W*TMath::Sin(thetaAng)*TMath::Sin(phiAng)/2.0 ) * TMath::Cos( k*L*TMath::Sin(thetaAng)*TMath::Cos(phiAng)/2 ) * TMath::Cos(phiAng) / (k*W*TMath::Sin(thetaAng)*TMath::Sin(phiAng)/2);
+  double premult = TMath::Sin(k * W * TMath::Sin(thetaAng) * TMath::Sin(phiAng) / 2.0) * TMath::Cos(k * L * TMath::Sin(thetaAng) * TMath::Cos(phiAng) / 2) * TMath::Cos(phiAng) / (k * W * TMath::Sin(thetaAng) * TMath::Sin(phiAng) / 2);
   thetaHat *= premult;
   return thetaHat;
 }
 
-TVector3 rad::PatchAntenna::GetEPhi(const TVector3 electronPosition) {
+TVector3 rad::PatchAntenna::GetEPhi(const TVector3 electronPosition)
+{
   TVector3 phiHat = GetPhiHat(electronPosition);
   double thetaAng = GetTheta(electronPosition);
   double phiAng = GetPhi(electronPosition);
   const double k = 2 * TMath::Pi() * centralFreq / TMath::C();
 
-  double premult = -1 * TMath::Sin( k*W*TMath::Sin(thetaAng)*TMath::Sin(phiAng)/2.0 ) * TMath::Cos( k*L*TMath::Sin(thetaAng)*TMath::Cos(phiAng)/2 ) * TMath::Cos(thetaAng)*TMath::Sin(phiAng) / (k*W*TMath::Sin(thetaAng)*TMath::Sin(phiAng)/2);
+  double premult = -1 * TMath::Sin(k * W * TMath::Sin(thetaAng) * TMath::Sin(phiAng) / 2.0) * TMath::Cos(k * L * TMath::Sin(thetaAng) * TMath::Cos(phiAng) / 2) * TMath::Cos(thetaAng) * TMath::Sin(phiAng) / (k * W * TMath::Sin(thetaAng) * TMath::Sin(phiAng) / 2);
   phiHat *= premult;
   return phiHat;
-}
-
-double rad::PatchAntenna::GetHEff() {
-  double LEff = TMath::C() / (2*GetCentralFrequency());
-  return LEff;
 }
--- Antennas/PatchAntenna.h
+++ Antennas/PatchAntenna.h
@@ -9,26 +9,41 @@
 #include "Antennas/IAntenna.h"
 #include "TVector3.h"
 
-namespace rad {
-
-  class PatchAntenna : public IAntenna {
+namespace rad
+{
+  class PatchAntenna : public IAntenna
+  {
 
   private:
-    double L;
-    double W;
-    double relativePerm;
-    
+    double L;            // Length of the patch (in metres)
+    double W;            // Width of the patch antenna (in metres)
+    double H;            // Height of the substrate (in metres)
+    double relativePerm; // Relative permettivity
+    double LEff;         // Effective length (in metres)
+
   public:
-    PatchAntenna(TVector3 antPos, TVector3 antXAx, TVector3 antZAx,
-		 const double length, const double width, const double permittivity=1.0, double delay=0.0);
-    
+    PatchAntenna(TVector3 antPos, TVector3 antXAx, TVector3 antYAx,
+                 double width, double height, double f0,
+                 double permittivity = 1.0, double delay = 0.0);
+
+    /// Calculates the electric field of the antenna in the theta direction
+    /// \param electronPosition Electron position (in metres)
+    /// \return The electric field (in V/m)
     TVector3 GetETheta(const TVector3 electronPosition);
+
+    /// Calculates the electric field of the antenna in the phi direction
+    /// \param electronPosition Electron position (in metres)
+    /// \return The electric field (in V/m)
     TVector3 GetEPhi(const TVector3 electronPosition);
 
-    double GetHEff();
+    double GetHEff() { return LEff; }
+
+    double GetL() { return L; }
+
+    double GetH() {return H; }
+
+    double GetW() { return W; }
   };
 }
-
-
 
 #endif
