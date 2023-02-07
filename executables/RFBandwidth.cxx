e0d78a8f233169e0d6d64013c565fce72a6b9c3a
Seb Jones <jonesseb100@gmail.com>
2023-02-07 14:11:40 +0000

    Loop over different background fields and test

--- executables/RFBandwidth.cxx
+++ executables/RFBandwidth.cxx
@@ -36,99 +36,113 @@ int main(int argc, char *argv[])
   cout << "dThetaMax = " << dThetaMax * 180 / TMath::Pi() << "degrees\n";
   cout << "Min. pitch angle = " << trapAngleMin * 180 / TMath::Pi() << " degrees\n";
 
-  const double bkgB{1.0};                                   // Background field in T
-  const double trapB{bkgB / pow(cos(dThetaMax), 2) - bkgB}; // Trapping field in T
-  cout << "Trap depth = " << trapB * 1e3 << " mT\n";
+  const double bkgBMin{0.5};
+  const double bkgBMax{1.0};
+  const int nbkgBPnts{6};
 
-  const double coilRadius{0.1};                           // Metres
-  const double coilCurrent{2 * trapB * coilRadius / MU0}; // Amps
-  const double trapLength{1.0};                           // Metres
-
-  BathtubField *field1m = new BathtubField(coilRadius, coilCurrent,
-                                           -trapLength / 2, trapLength / 2,
-                                           TVector3(0, 0, bkgB));
-  const TVector3 centralField{field1m->evaluate_field_at_point(TVector3(0, 0, 0))};
-  cout << "Central field = " << centralField.Mag() << "T\n";
-
-  const double pitchAngleStart{1.001 * trapAngleMin};
-  const double pitchAngleEnd{90 * TMath::Pi() / 180};
-  const int nPnts{40};
-  const double simTime{2e-6};
-  const double simStepSize{1e-12};
-
-  // Electron kinematics
-  const double electronKE{18600}; // eV
-  const double electronSpeed{GetSpeedFromKE(electronKE, ME)};
-  const double tau = 2 * R_E / (3 * TMath::C());
-
-  TGraph *grBMean = new TGraph();
-  setGraphAttr(grBMean);
-  grBMean->SetMarkerStyle(20);
-  grBMean->SetTitle(Form("1 m long bathtub trap, B_{bkg} = %.1f T, #Delta B = %.1f mT; #theta [degrees]; B_{mean} [T]", bkgB, trapB * 1e3));
-
-  TGraph *grF = new TGraph();
-  setGraphAttr(grF);
-  grF->SetMarkerStyle(20);
-  grF->SetTitle(Form("1 m long bathtub trap, B_{bkg} = %.1f T, #Delta B = %.1f mT; #theta [degrees]; f [Hz]", bkgB, trapB * 1e3));
-
-  TGraph *grDeltaF = new TGraph();
-  setGraphAttr(grDeltaF);
-  grDeltaF->SetMarkerStyle(20);
-  grDeltaF->SetTitle(Form("1 m long bathtub trap, B_{bkg} = %.1f T, #Delta B = %.1f mT; #theta [degrees]; #Delta f [Hz]", bkgB, trapB * 1e3));
-
-  for (int iPnt{0}; iPnt < nPnts; iPnt++)
+  for (int iBkg{0}; iBkg < nbkgBPnts; iBkg++)
   {
-    double thisAngle{pitchAngleStart + (pitchAngleEnd - pitchAngleStart) * double(iPnt) / double(nPnts - 1)};
-    TVector3 V0(electronSpeed * sin(thisAngle), 0, electronSpeed * cos(thisAngle));
-    const double gyroradius{GetGyroradius(V0,
-                                          field1m->evaluate_field_at_point(TVector3(0, 0, 0)),
-                                          ME)};
-    TVector3 X0(0, -gyroradius, 0);
-    TString trackFile{Form("/home/sjones/work/qtnm/outputs/RFBandwidth/track%d.root", iPnt)};
-    ElectronTrajectoryGen traj(trackFile, field1m, X0, V0, simStepSize,
-                               simTime, 0.0, tau);
-    traj.GenerateTraj();
+    // Background field in T
+    double bkgB{bkgBMin +
+                (bkgBMax - bkgBMin) * double(iBkg) / double(nbkgBPnts - 1)};
+    const double trapB{bkgB / pow(cos(dThetaMax), 2) - bkgB}; // Trapping field in T
+    cout << "Background field = " << bkgB << " T,\t Trap depth = " << trapB * 1e3 << " mT\n";
 
-    // Now open track file
-    TFile *fin = new TFile(trackFile, "READ");
-    TTree *tr = (TTree *)fin->Get("tree");
-    double xPos, yPos, zPos;
-    tr->SetBranchAddress("xPos", &xPos);
-    tr->SetBranchAddress("yPos", &yPos);
-    tr->SetBranchAddress("zPos", &zPos);
+    const double coilRadius{0.1};                           // Metres
+    const double coilCurrent{2 * trapB * coilRadius / MU0}; // Amps
+    const double trapLength{1.0};                           // Metres
 
-    double bMean{0};
-    // Loop over tree entries
-    for (int e{0}; e < tr->GetEntries(); e++)
+    BathtubField *field1m = new BathtubField(coilRadius, coilCurrent,
+                                             -trapLength / 2, trapLength / 2,
+                                             TVector3(0, 0, bkgB));
+    const TVector3 centralField{field1m->evaluate_field_at_point(TVector3(0, 0, 0))};
+    cout << "Central field = " << centralField.Mag() << "T\n";
+
+    const double pitchAngleStart{1.001 * trapAngleMin};
+    const double pitchAngleEnd{90 * TMath::Pi() / 180};
+    const int nPnts{20};
+    const double simTime{2e-6};
+    const double simStepSize{1e-12};
+
+    // Electron kinematics
+    const double electronKE{18600}; // eV
+    const double electronSpeed{GetSpeedFromKE(electronKE, ME)};
+    const double tau = 2 * R_E / (3 * TMath::C());
+
+    TGraph *grBMean = new TGraph();
+    setGraphAttr(grBMean);
+    grBMean->SetMarkerStyle(20);
+    grBMean->SetTitle(Form("1 m long bathtub trap, B_{bkg} = %.1f T, #Delta B = %.1f mT; #theta [degrees]; B_{mean} [T]", bkgB, trapB * 1e3));
+
+    TGraph *grF = new TGraph();
+    setGraphAttr(grF);
+    grF->SetMarkerStyle(20);
+    grF->SetTitle(Form("1 m long bathtub trap, B_{bkg} = %.1f T, #Delta B = %.1f mT; #theta [degrees]; f [Hz]", bkgB, trapB * 1e3));
+
+    TGraph *grDeltaF = new TGraph();
+    setGraphAttr(grDeltaF);
+    grDeltaF->SetMarkerStyle(20);
+    grDeltaF->SetTitle(Form("B_{bkg} = %.1f T, #Delta B = %.1f mT; #theta [degrees]; #Delta f [Hz]", bkgB, trapB * 1e3));
+    grDeltaF->SetMarkerColor(52 + iBkg * 3);
+
+    for (int iPnt{0}; iPnt < nPnts; iPnt++)
     {
-      tr->GetEntry(e);
-      TVector3 ePos(xPos, yPos, zPos);
-      bMean += field1m->evaluate_field_magnitude(ePos);
+      double thisAngle{pitchAngleStart + (pitchAngleEnd - pitchAngleStart) * double(iPnt) / double(nPnts - 1)};
+      TVector3 V0(electronSpeed * sin(thisAngle), 0, electronSpeed * cos(thisAngle));
+      const double gyroradius{GetGyroradius(V0,
+                                            field1m->evaluate_field_at_point(TVector3(0, 0, 0)),
+                                            ME)};
+      TVector3 X0(0, -gyroradius, 0);
+      TString trackFile{Form("/home/sjones/work/qtnm/outputs/RFBandwidth/track%d.root", iPnt)};
+      ElectronTrajectoryGen traj(trackFile, field1m, X0, V0, simStepSize,
+                                 simTime, 0.0, tau);
+      traj.GenerateTraj();
+
+      // Now open track file
+      TFile *fin = new TFile(trackFile, "READ");
+      TTree *tr = (TTree *)fin->Get("tree");
+      double xPos, yPos, zPos;
+      tr->SetBranchAddress("xPos", &xPos);
+      tr->SetBranchAddress("yPos", &yPos);
+      tr->SetBranchAddress("zPos", &zPos);
+
+      double bMean{0};
+      // Loop over tree entries
+      for (int e{0}; e < tr->GetEntries(); e++)
+      {
+        tr->GetEntry(e);
+        TVector3 ePos(xPos, yPos, zPos);
+        bMean += field1m->evaluate_field_magnitude(ePos);
+      }
+      bMean /= double(tr->GetEntries());
+      cout << iPnt << ":\t bMean = " << bMean << " T\n";
+
+      double f{CalcCyclotronFreq(electronKE, bMean)};
+      grBMean->SetPoint(iPnt, thisAngle * 180 / TMath::Pi(), bMean);
+      grF->SetPoint(iPnt, thisAngle * 180 / TMath::Pi(), f);
+      grDeltaF->SetPoint(iPnt, thisAngle * 180 / TMath::Pi(), f);
+
+      delete tr;
+      fin->Close();
+      delete fin;
     }
-    bMean /= double(tr->GetEntries());
-    cout << iPnt << ":\t bMean = " << bMean << " T\n"
-         << endl;
 
-    double f{CalcCyclotronFreq(electronKE, bMean)};
-    grBMean->SetPoint(iPnt, thisAngle * 180 / TMath::Pi(), bMean);
-    grF->SetPoint(iPnt, thisAngle * 180 / TMath::Pi(), f);
-    grDeltaF->SetPoint(iPnt, thisAngle * 180 / TMath::Pi(), f);
+    for (int n{0}; n < grDeltaF->GetN(); n++)
+    {
+      double y{grDeltaF->GetPointY(n)};
+      grDeltaF->SetPointY(n, y - grDeltaF->GetPointY(grDeltaF->GetN() - 1));
+    }
 
-    delete tr;
-    fin->Close();
-    delete fin;
-  }
+    fout->cd();
+    grBMean->Write(Form("grBMean%d", iBkg));
+    grF->Write(Form("grF%d", iBkg));
+    grDeltaF->Write(Form("grDeltaF%d", iBkg));
 
-  for (int n{0}; n < grDeltaF->GetN(); n++)
-  {
-    double y{grDeltaF->GetPointY(n)};
-    grDeltaF->SetPointY(n, y - grDeltaF->GetPointY(grDeltaF->GetN() - 1));
-  }
-
-  fout->cd();
-  grBMean->Write("grBMean");
-  grF->Write("grF");
-  grDeltaF->Write("grDeltaF");
+    delete grBMean;
+    delete grF;
+    delete grDeltaF;
+    cout << "\n";
+  } // Loop over background fields
 
   fout->Close();
   delete fout;
